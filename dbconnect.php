28a59ebcbc7b6a1c077abf2aab78e705bd5c77a2
mayorovtn <56407348+mayorovtn@users.noreply.github.com>
2021-10-13 10:45:04 +0800

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+﻿<?php 
+require "libs/rb-mysql.php";
+
+R::setup ('mysql:host=localhost; dbname=mydb', 'root', '848254900789'); //подключаем базу данных
+
+if(!R::testConnection()) die('Отсутствует соединение с БД'); // проверяем подключение к БД
+
+//session_start(); // создаем сессию для авторизации
+?>
---
+++ viewnomin.php
@@ -0,0 +1,17 @@
+﻿<?php
+require "dbconnect.php";
+/*
+$users = R::dispense('users');
+
+$users->login = "bns.com";
+$users->password = "123456789"; 
+
+R::store($users);
+*/
+
+$users = R::findAll('users');
+foreach($users as $user){
+	//echo "{$user[login]}</br>";
+	echo $user->login. '</br>';
+}
+?>
